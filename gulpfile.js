1f3b13e4725273f6c2d3b335b5d1968bf8d749bc
Yaroslav Rukavets <yaroslav_rukavets@ukr.net>
2018-02-22 20:43:28 +0200

    Added docs directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
 var gulp = require('gulp');
 var fs = require("fs");
 var {promisify} = require("util");
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
 const readFile = promisify(fs.readFile);
-
-gulp.task('default', ['less', "mustache"], function() {
-    gulp.watch('./src/styles/**/*.less', ['less']);
-    gulp.watch("./src/images/*.*", ["copy"]);
-    gulp.watch([
-        './src/data/data.json',
-        './src/**/*.html'
-    ], ['mustache']);
-});
 
 var less = require('gulp-less');
 var path = require('path');
@@ -21,27 +12,36 @@ gulp.task('less', function () {
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ]
         }))
-        .pipe(gulp.dest('./docs/styles'));
+        .pipe(gulp.dest('./docs/styles'))
+        .pipe(browserSync.stream());
 });
 
 var mustache = require("gulp-mustache");
 gulp.task("mustache", async () => {
     const data = JSON.parse(await readFile("./src/data/data.json", "utf-8"));
 
-return gulp.src("./src/**/*.html")
-    .pipe(mustache(data))
-    .pipe(gulp.dest("./docs"));
+    return gulp.src("./src/**/*.html")
+        .pipe(mustache(data))
+        .pipe(gulp.dest("./docs"));
 })
 
 gulp.task('copy', function () {
     gulp.src('./src/images/*.*')
         .pipe(gulp.dest('./docs/images/'));
 });
-gulp.task('server', function(){
+
+
+gulp.task('default', ["less", "mustache"], function () {
     browserSync.init({
-        server:{
-            baseDir: "docs/"
-        },
-        files: ['docs/*.html','docs/*.styles','docs/images/*.*']
+        server: "./docs"
     });
+
+    gulp.watch('./src/styles/**/*.less', ['less']);
+    gulp.watch("./src/images/*.*", ["copy"]);
+
+    gulp.watch([
+        './src/data/data.json',
+        './src/**/*.html'
+    ], ['mustache'])
+        .on('change', browserSync.reload);
 });
